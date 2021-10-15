e560b90cb2d4383cd84a817378d5b78affef94c3
kai0808-S <81606198+kai0808-S@users.noreply.github.com>
2021-10-15 17:36:25 +0900

    change2-3

--- section2-3/sketch.js
+++ section2-3/sketch.js
@@ -5,9 +5,11 @@ function setup(){
   createCanvas(100, 100);
   for(let i = 0; i < 10; i++){
     for(let j = 0; j < 10; j++){
-      fill(j % 2 == 0 ? 255 : 0); // ここを変えます
+    if(i%2==0)  {fill(200,60,78);
+    rect(i * 10, j * 10, 5, 5);}
+    else{fill(50);
+      rect(i * 10, j * 10, 5, 5);}
       console.log(i, j);
-      rect(i * 10, j * 10, 5, 5);
     }
   }
 }
