4aec09ae114d0ea435e83b81f181a794acd669a6
kai0808-S <81606198+kai0808-S@users.noreply.github.com>
2021-10-15 18:38:11 +0900

    change section2-q1

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -1,6 +1,8 @@
 // 小手調べ
 function setup(){
-  for(let i = 0; i < 10; i++){
-    // BLANK[1]
+  for(let i = 100; i > 0; i-=10){
+  if(i>50){stroke(255,0,0);}
+  else{stroke(0,0,255);}
+  ellipse(50,50,i);  // BLANK[1]
   }
 }
