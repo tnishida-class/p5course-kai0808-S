15b0231da135977db3a810381a765c19d1996c9a
kai0808-S <81606198+kai0808-S@users.noreply.github.com>
2021-10-15 20:34:14 +0900

    change section2-q3

--- section2-q3/sketch.js
+++ section2-q3/sketch.js
@@ -9,27 +9,33 @@ function setup() {
   background(255);
   stroke(255);
   strokeWeight(3);
+fill(black);
+ ellipse(200,200,width);
+fill(cream);
+ ellipse(200,200,320,320);
+ let x=1/10; let y=1/5;
+ while(x<2)
+ { fill(red);
+   arc(200,200,320,320,x*PI,y*PI);
+   fill(black);
+   arc(200,200,290,290,x*PI,y*PI);
+   fill(red);
+   arc(200,200,160,160,x*PI,y*PI);
+   fill(black);
+   arc(200,200,130,130,x*PI,y*PI);
 
-  const cx = width / 2; // 中心は (cx, cy)
-  const cy = height / 2;
-  const maxR = min(width, height); // 大きさは幅と高さのうち小さい方
+   fill(green);
+     arc(200,200,320,320,x*PI+1/10*PI,y*PI+1/10*PI);
+     fill(cream);
+     arc(200,200,290,290,x*PI+1/10*PI,y*PI+1/10*PI);
+     fill(green);
+     arc(200,200,160,160,x*PI+1/10*PI,y*PI+1/10*PI);
+     fill(cream);
+     arc(200,200,130,130,x*PI+1/10*PI,y*PI+1/10*PI);
+   x+=1/5; y+=1/5; }
 
-  drawCircle(black, maxR);
-  drawArcs(green, red, maxR * 0.8);
-  // BLANK[1] (hint: drawArcs x 3, drawCircle x 1)
-  drawCircle(red, maxR * 0.05);
-}
-
-function drawCircle(c, r){
-  fill(c);
-  ellipse(cx, cy, r, r);
-}
-
-function drawArcs(c1, c2, r) {
-  for (let i = 0; i < 20; i++) {
-    let start = TWO_PI / 20 * i;
-    let stop = TWO_PI / 20 * (i + 1);
-    fill(i % 2 == 0 ? c1 : c2);
-    arc(cx, cy, r, r, start, stop, PIE);
-  }
-}
+  fill(green);
+    ellipse(200,200,40);
+  fill(red);
+    ellipse(200,200,25);
+ }
