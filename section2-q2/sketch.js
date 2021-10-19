cf54911ec20e0bfa570c2aea4aa38790a70fef6c
kai0808-S <81606198+kai0808-S@users.noreply.github.com>
2021-10-19 12:47:47 +0900

    change section2-q2

--- section2-q2/sketch.js
+++ section2-q2/sketch.js
@@ -1,11 +1,17 @@
 // チェッカー
 function setup() {
   createCanvas(200, 200);
-  let size = width / 8;
   noStroke();
+  let size = width/8;
   for(let i = 0; i < 8; i++){
-    for(let j = 0; j < 8; j++){
-      // BLANK[1] (hint: rectのx座標は size * i, y座標はsize * j)
-    }
-  }
+  for(let j = 0; j < 8; j++){
+   if(i%2==0&& j%2==1){ fill('#708090');
+  rect(i*size,j*size,size,size);}
+  if(i%2==0 && j==1){fill(255,0,0);ellipse(i*size+1/2*size,j*size+1/2*size,size,size);}
+  if(i%2==0&&j==5 || i%2==0&&j==7){fill(25);ellipse(i*size+1/2*size,j*size+1/2*size,size,size);}
+  if(i%2==1 && j%2==0){fill('#708090');rect(i*size,j*size,size,size);}
+  if(i%2==1&&j==0 || i%2==1&&j==2){fill(255,0,0);ellipse(i*size+1/2*size,j*size+1/2*size,size,size);}
+  if(i%2==1&&j==6){fill(25);ellipse(i*size+1/2*size,j*size+1/2*size,size,size);}
+ }
+}
 }
