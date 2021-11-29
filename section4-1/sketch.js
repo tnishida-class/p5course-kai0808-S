c7334de86f2823e0fa6fe966cc0a27b55d667568
kai0808-S <81606198+kai0808-S@users.noreply.github.com>
2021-11-29 22:51:51 +0900

    change 4-1

--- section4-1/sketch.js
+++ section4-1/sketch.js
@@ -19,32 +19,35 @@ function setup(){
 
   // ここから平均・最大・最小を求めます
   let average, largest, smallest;
-average=sum/scores.length
+average=sum/scores.length;
+console.log(average);
   largest = 0;
   for(let i = 0; i < scores.length; i++){
-   if(scores(i)>largest){largest=scores(i)};
-    // BLANK[2]　ヒント：今までの最大値 largest と scores[i] を比較する
+   if(scores[i]>largest){largest=scores[i]};
   }
-
+console.log(largest);
   smallest = 100;
   for(let i = 0; i < scores.length; i++){
-    // BLANK[3]　ヒント：最小値とだいたい同じ
+  if(scores[i]<smallest){smallest=scores[i]};
   }
-
+console.log(smallest);
   // ここから棒グラフを描いていきます。まずは背景に横線をn本引く
   const n = 10;
-  for(let i = 0; i < n; i++){ line(0, height * i / n, width, height * i / n); }
+  for(let i = 0; i < n; i++){ line(0, height * i / n, width, height * i / n);}
 
   noStroke();
 
   for(let i = 0; i < scores.length; i++){
     const dx = width / scores.length;
     const h = height * scores[i] / 100;
-    // BLANK[4] ヒント: 条件分岐を使って色を変更します
+    fill(0);
+    if(scores[i]==largest){fill('red')};
+    if(scores[i]==smallest){fill('blue')};// BLANK[4] ヒント: 条件分岐を使って色を変更します
     rect(i * dx + 2, height - h, dx - 4, h);
     fill(0);
     text(scores[i].toPrecision(3), i * dx, height - h);
-  }
-
+  };
+  stroke('green');
+line(0,height-height*average/100,width,height-height*average/100);
   // BLANK[5] 平均点の線を引きます
 }
